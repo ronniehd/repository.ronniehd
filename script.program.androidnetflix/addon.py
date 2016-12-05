1ec7f2ea8e886fffc7f2f05d9b924943e82c3da0
RonnieHD <ronniehd@users.noreply.github.com>
2016-12-04 19:28:20 -0500

    Netflix android shortcut

---
+++ script.program.androidnetflix/addon.py
@@ -0,0 +1,15 @@
+import xbmc
+import xbmcgui
+import os
+
+appname="Vevo"
+packagename="com.vevo"
+
+def launcher():
+	packagepath = os.popen('pm path %s' % packagename).read()
+	if packagepath:
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename)
+	else:
+		xbmcgui.Dialog().ok('App No Instalada', 'Esto es solo un atajo.  Por favor instale la app "%s" (%s) e intente de nuevo.' % (appname, packagename))
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("com.android.vending","android.intent.action.VIEW",,"market://details?id=com.vevo")')
+launcher()
